1bb03274eb1a920a1f4ffdd5aae7d59383181170
macribo <roibeard.mac.unfraidh@gmail.com>
2016-11-18 07:14:17 +0100

    try to create a merge conflict

--- synflicting.js
+++ synflicting.js
@@ -4,3 +4,5 @@ function conflicts(){
 alert("this is different and conflicting instruction.");}
 conflicts();
 //this should work fine on it's own, but cause problems when merged with the local synfliction file.
+//this has to cause a conflict, right?
+//two different files with the same name?
