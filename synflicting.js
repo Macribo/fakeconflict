b0dd4ce73e985bb50cc4a7a3b81891d131762b3c
macribo <roibeard.mac.unfraidh@gmail.com>
2016-11-18 06:36:54 +0100

    force edit collision

---
+++ synflicting.js
@@ -0,0 +1,4 @@
+function conflicts(){
+console.log("this is a different and conflicting instruction.");}
+conflicts();
+//this should work fine on it's own, but cause problems when merged with the local synfliction file.
