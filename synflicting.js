d50d12085746954697420960ee1ac392d9546af2
macribo <roibeard.mac.unfraidh@gmail.com>
2016-11-18 10:42:44 +0100

    changed something

--- synflicting.js
+++ synflicting.js
@@ -6,3 +6,4 @@ conflicts();
 //this should work fine on it's own, but cause problems when merged with the local synfliction file.
 //this has to cause a conflict, right?
 //2 different files with the same name?
+
